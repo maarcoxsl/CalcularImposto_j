aece3b22e1c5bf3acec0f105c306c04d9642ccac
marcosxsl <marcolima.05@outlook.com>
2026-08-07 11:43:10 -0300

    Calcular imposto V3

--- cImposto_j/src/Main.java
+++ cImposto_j/src/Main.java
@@ -10,18 +10,32 @@ void main(){
     var salarioMensal = entradaUsuario.nextDouble();
     var salarioAnual = salarioMensal * 12;
 
+    System.out.println("Quantos dependentes você possui ?\nInsira de 1 a 5");
+    var dependentes = entradaUsuario.nextInt();
+    var comDependente = salarioAnual - (dependentes * 1500);
+
+
     if (salarioAnual >= 0 && salarioAnual <= 22000.57){
+        System.out.println("Numero de dependentes :" + dependentes);
         System.out.println(nomeUsuario + "," + " " + "você está insento de pagar imposto de renda");
+        System.out.println("Imposto de renda devido: " + " " + comDependente);
     } else if (salarioAnual >= 33919.30 && salarioAnual <= 45012.60){
         var totalPagar = salarioAnual * 0.75;
-        System.out.println("Total a pagar de imposto de renda: " + totalPagar);
+        System.out.println("Numero de dependentes :" + dependentes);
+        System.out.println("Imposto de renda devido com dependentes: " + " " + comDependente);
+        System.out.println("Imposto de renda devido: " + totalPagar);
+
     } else if (salarioAnual >= 45012.60 && salarioAnual <= 55976.16){
         var totalPagar1 = salarioAnual * 2.25;
-        System.out.println(nomeUsuario + "," + "Total a pagar de imposto de renda: " + totalPagar1);
+        System.out.println("Numero de dependentes :" + dependentes);
+        System.out.println("Imposto de renda devido com dependentes: " + " " + comDependente);
+        System.out.println("Imposto de renda devido: " + " " + totalPagar1);
 
     } else if (salarioAnual > 55976.16){
         var totalPagar2 = salarioAnual * 2.75;
-        System.out.println( nomeUsuario+","+"Total a pagar de imposto de renda: " + totalPagar2);
+        System.out.println("Numero de dependentes :" + dependentes);
+        System.out.println("Imposto de renda devido com dependentes: " + " " + comDependente);
+        System.out.println("Imposto de renda devido: " + " " + totalPagar2);
     } else {
         System.out.println("Codigo invalido !!!");
     }
